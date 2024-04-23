e615d433e9cb804f800af75e5028891f70924210
hallo-gamify <dzung@gamifystudios.co>
2024-04-23 18:46:26 +0700

    continue inheritancify

--- assets/scripts/cocoseus/modifier/Inheritancify.ts
+++ assets/scripts/cocoseus/modifier/Inheritancify.ts
@@ -1,20 +1,37 @@
-import { _decorator, Component, Node } from 'cc';
+import { _decorator, Component, Constructor, Node } from 'cc';
 const { ccclass, property } = _decorator;
 
-// @ccclass('Inheritancify')
-// export class Inheritancify extends Component {
-//     start() {
-
-//     }
-
-//     update(deltaTime: number) {
-        
-//     }
-// }
-
-
-export function Inheritancify(lifecycleClass, ){
+@ccclass('InheritanceInvoker')
+export class InheritanceInvoker {
     
 }
 
 
+
+/**
+ * 
+ * @param constructor 
+ * @param additionalConstructor 
+ * @returns 
+ */
+type validateTBase<T> = T extends { __props__: unknown, __values__: unknown } ? Constructor<T> : any;
+type ReturnModifier<T> = T extends Component? Constructor<T> : InheritanceInvoker;
+
+export function Inheritancify<TModifier>(executeMethod:(...arg:any)=>Constructor<Component>, invokerCtor:Constructor<InheritanceInvoker>):(<TBase>(base:validateTBase<TBase>)=>ReturnModifier<TBase&TModifier>){    
+    
+    return function<TBase>(base:validateTBase<TBase>):ReturnModifier<TBase&TModifier>{
+        if(base['__props__'] && base['__values__']){            // 
+            if(isValid(base as Constructor, invokerCtor)) return base as unknown as ReturnModifier<TBase&TModifier>;
+            const superClass:Constructor = executeMethod.apply(this, Array.from(arguments));        
+            // superClass[State] = new stateConstructor(modifier.name);
+            return superClass as unknown as ReturnModifier<TBase&TModifier>;
+        }else{
+            // return useState.call(stateConstructor, base)
+        }   
+    } as <TBase>(base:validateTBase<TBase>)=>ReturnModifier<TBase&TModifier>
+}
+
+
+function isValid(baseCtor:Constructor, invokerCtor:Constructor<InheritanceInvoker>):boolean{
+    return
+}
