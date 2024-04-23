d40ec9cd0fe4340304d768832afbd88d7f357df2
hallo-gamify <dzung@gamifystudios.co>
2024-04-23 18:37:49 +0700

    start inheritancify

---
+++ assets/scripts/cocoseus/modifier/Inheritancify.ts
@@ -0,0 +1,20 @@
+import { _decorator, Component, Node } from 'cc';
+const { ccclass, property } = _decorator;
+
+// @ccclass('Inheritancify')
+// export class Inheritancify extends Component {
+//     start() {
+
+//     }
+
+//     update(deltaTime: number) {
+        
+//     }
+// }
+
+
+export function Inheritancify(lifecycleClass, ){
+    
+}
+
+
